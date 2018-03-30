069fcdb58ded3931e0fee2e877411ef2a4152518
Petri Kainulainen <petri.kainulainen@gmail.com>
2018-03-30 11:51:22 +0300

    Add assertThatThrownBy() example

--- unit-tests/writing-assertions/assertj/src/test/java/net/petrikainulainen/junit5/ExceptionAssertionTest.java
+++ unit-tests/writing-assertions/assertj/src/test/java/net/petrikainulainen/junit5/ExceptionAssertionTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.catchThrowable;
 
 /**
@@ -17,46 +18,82 @@ import static org.assertj.core.api.Assertions.catchThrowable;
 class ExceptionAssertionTest {
 
     @Nested
-    @DisplayName("When the system under test throws the correct exception")
-    class WhenSystemUnderTestThrowsException {
+    @DisplayName("When we write assertions directly to the thrown exception")
+    class WhenWeWriteAssertionsForThrownException {
 
-        @Test
-        @DisplayName("Should throw the correct exception")
-        void shouldThrowCorrectException() {
-            final Throwable thrown = catchThrowable(() -> { throw new NullPointerException(); });
-            assertThat(thrown).isExactlyInstanceOf(NullPointerException.class);
+        @Nested
+        @DisplayName("When the system under test throws the correct exception")
+        class WhenSystemUnderTestThrowsException {
+
+            @Test
+            @DisplayName("Should throw the correct exception")
+            void shouldThrowCorrectException() {
+                assertThatThrownBy(() -> { throw new NullPointerException(); })
+                        .isExactlyInstanceOf(NullPointerException.class);
+            }
         }
 
-        @Test
-        @DisplayName("Should throw the correct exception (with custom error message)")
-        void shouldThrowCorrectExceptionWithCustomErrorMessage() {
-            final Throwable thrown = catchThrowable(() -> { throw new NullPointerException(); });
-            assertThat(thrown)
-                    .overridingErrorMessage(
-                            "Expected the code to throw a NullPointerException but it threw: %s",
-                            thrown.getClass().getSimpleName()
-                    )
-                    .isExactlyInstanceOf(NullPointerException.class);
+        @Nested
+        @DisplayName("When the system under test throws an exception that has the correct message")
+        class WhenSystemUnderTestThrowsExceptionWithCorrectMessage {
+
+            private final String ERROR_MESSAGE = "Hello World!";
+
+            @Test
+            @DisplayName("Should throw an exception that has the correct message")
+            void shouldThrowAnExceptionWithCorrectMessage() {
+                assertThatThrownBy(() -> { throw new NullPointerException(ERROR_MESSAGE); })
+                        .hasMessage(ERROR_MESSAGE);
+            }
         }
     }
 
     @Nested
-    @DisplayName("When the system under test throws an exception that has the correct message")
-    class WhenSystemUnderTestThrowsExceptionWithCorrectMessage {
+    @DisplayName("When we catch the thrown exception object")
+    class WhenWeCatchThrownExceptionObject {
 
-        private final String ERROR_MESSAGE = "Hello World!";
+        @Nested
+        @DisplayName("When the system under test throws the correct exception")
+        class WhenSystemUnderTestThrowsException {
 
-        @Test
-        @DisplayName("Should throw an exception that has the correct message")
-        void shouldThrowAnExceptionWithCorrectMessage() {
-            final Throwable thrown = catchThrowable(() -> { throw new NullPointerException(ERROR_MESSAGE); });
-            assertThat(thrown.getMessage())
-                    .overridingErrorMessage(
-                            "Expected the error message of the exception to be: %s but it was: %s",
-                            ERROR_MESSAGE,
-                            thrown.getMessage()
-                    )
-                    .isEqualTo(ERROR_MESSAGE);
+            @Test
+            @DisplayName("Should throw the correct exception")
+            void shouldThrowCorrectException() {
+                final Throwable thrown = catchThrowable(() -> { throw new NullPointerException(); });
+                assertThat(thrown).isExactlyInstanceOf(NullPointerException.class);
+            }
+
+            @Test
+            @DisplayName("Should throw the correct exception (with custom error message)")
+            void shouldThrowCorrectExceptionWithCustomErrorMessage() {
+                final Throwable thrown = catchThrowable(() -> { throw new NullPointerException(); });
+                assertThat(thrown)
+                        .overridingErrorMessage(
+                                "Expected the code to throw a NullPointerException but it threw: %s",
+                                thrown.getClass().getSimpleName()
+                        )
+                        .isExactlyInstanceOf(NullPointerException.class);
+            }
+        }
+
+        @Nested
+        @DisplayName("When the system under test throws an exception that has the correct message")
+        class WhenSystemUnderTestThrowsExceptionWithCorrectMessage {
+
+            private final String ERROR_MESSAGE = "Hello World!";
+
+            @Test
+            @DisplayName("Should throw an exception that has the correct message")
+            void shouldThrowAnExceptionWithCorrectMessage() {
+                final Throwable thrown = catchThrowable(() -> { throw new NullPointerException(ERROR_MESSAGE); });
+                assertThat(thrown.getMessage())
+                        .overridingErrorMessage(
+                                "Expected the error message of the exception to be: %s but it was: %s",
+                                ERROR_MESSAGE,
+                                thrown.getMessage()
+                        )
+                        .isEqualTo(ERROR_MESSAGE);
+            }
         }
     }
 }
